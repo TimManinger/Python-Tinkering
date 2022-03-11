f6a49040be4b5218e8603577566f321787d6df99
Tim <tvmwd6@umsystem.edu>
2022-03-11 08:27:19 -0600

    did some impractical projects

---
+++ impractical_python_projects/palinGRAM/palingrams.py
@@ -0,0 +1,20 @@
+import load_dict
+import sys
+
+word_list = set(load_dict.load("../words.txt"))
+
+def find_palingrams():
+    pali_list = []
+    for word in word_list:
+        end = len(word)
+        rev_word = word[::-1]
+        if end > 1:
+            for i in range(end):
+                if word[i:] == rev_word[:end-i] and rev_word[end-i:] in word_list:
+                    pali_list.append((word, rev_word[end-i:]))
+                if word[:i] == rev_word[end-i:] and rev_word[:end-i] in word_list:
+                    pali_list.append((rev_word[:end-i],word))
+    return pali_list
+
+palingrams = find_palingrams()
+print(palingrams)
